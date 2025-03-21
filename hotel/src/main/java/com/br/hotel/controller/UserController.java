62d879d9f9b0698d1d05fb2a248eba132cb1fe44
l1c4s <lucasferreirabarrosdallas@gmail.com>
2025-03-21 19:35:58 -0300

    Metodo para redirecionar para a pagina mainpage

--- hotel/src/main/java/com/br/hotel/controller/UserController.java
+++ hotel/src/main/java/com/br/hotel/controller/UserController.java
@@ -1,20 +1,20 @@
 package com.br.hotel.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.servlet.ModelAndView;
 
-import com.br.hotel.Repositorios.UserClienteRepositorio;
 
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 
-@RestController
+
+
+@Controller
 public class UserController {
 
     @GetMapping("/")
@@ -22,6 +22,14 @@ public class UserController {
         ModelAndView mv = new ModelAndView("index");
         return mv;
     }
+
+    @PostMapping("/mainpage")
+    public String postMethodName(@RequestBody String cpf, String senha) {
+        //TODO: process POST request
+        
+        return "redirect:/mainpage";
+    }
+    
     
     
 }
