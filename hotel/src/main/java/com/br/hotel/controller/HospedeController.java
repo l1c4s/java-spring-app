46ad942a1a7c0b702ffbe952701b9fe682e27037
l1c4s <lucasferreirabarrosdallas@gmail.com>
2025-03-21 19:34:49 -0300

    Ajustando o 'GetMapping'

--- hotel/src/main/java/com/br/hotel/controller/HospedeController.java
+++ hotel/src/main/java/com/br/hotel/controller/HospedeController.java
@@ -1,13 +1,13 @@
 package com.br.hotel.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HospedeController {
 
-    @GetMapping("/")
+    @GetMapping("/mainpage")
     public ModelAndView home(){
         ModelAndView mv = new ModelAndView("mainpage");
         return mv;
