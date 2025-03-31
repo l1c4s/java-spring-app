e60e3d88522ff991bae4129e4d9340a2a30c589d
l1c4s <lucasferreirabarrosdallas@gmail.com>
2025-03-31 17:17:46 -0300

    Inserções sql

---
+++ hotel/src/main/resources/templates/import.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO usuario (cpf, senha) VALUES ("lucas.br","123456");
+
+INSERT INTO hospedes (cpf, nome, telefone, usercliente_cpf)
+VALUES ('12345678901', 'João Silva', '(11)9876321', 'lucas.br');
