3925470fce30e478c275ee45d5773ac9baa10a3b
l1c4s <lucasferreirabarrosdallas@gmail.com>
2025-03-31 17:21:17 -0300

    Metodo para cadastrar o Usuario

--- hotel/src/main/java/com/br/hotel/controller/UserController.java
+++ hotel/src/main/java/com/br/hotel/controller/UserController.java
@@ -1,14 +1,25 @@
 package com.br.hotel.controller;
 
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.servlet.ModelAndView;
 
+import com.br.hotel.Repositorios.UserClienteRepositorio;
+import com.br.hotel.models.UserCliente;
+
+import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -17,6 +28,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Controller
 public class UserController {
 
+    @Autowired
+    private UserClienteRepositorio userrepositorio;
+
     @GetMapping("/")
     public ModelAndView home(){
         ModelAndView mv = new ModelAndView("index");
@@ -24,10 +38,18 @@ public class UserController {
     }
 
     @PostMapping("/mainpage")
-    public String postMethodName(@RequestBody String cpf, String senha) {
-        //TODO: process POST request
+    public ModelAndView cadastrar(@Valid UserCliente usercliente, BindingResult result) {
         
-        return "redirect:/mainpage";
+        if(result.hasErrors())
+            return new ModelAndView("index").addObject("usercliente", new UserCliente());
+        try{
+            userrepositorio.save(usercliente);
+        }catch(Exception e){
+            result.rejectValue("cpf", "Error.user", "erro ao salvar o usuario: "+e.getMessage());
+            return new ModelAndView("index").addObject("usercliente", new UserCliente());
+        }
+        
+        return new ModelAndView("redirect:/mainpage");
     }
     
     
