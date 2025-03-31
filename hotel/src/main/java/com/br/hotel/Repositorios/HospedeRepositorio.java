bcb4b86609cffce6730c0c868c3087d29fcb5c9c
l1c4s <lucasferreirabarrosdallas@gmail.com>
2025-03-31 17:28:06 -0300

    Ajustes

--- hotel/src/main/java/com/br/hotel/Repositorios/HospedeRepositorio.java
+++ hotel/src/main/java/com/br/hotel/Repositorios/HospedeRepositorio.java
@@ -3,11 +3,19 @@ package com.br.hotel.Repositorios;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.br.hotel.models.Hospede;
 
+@Repository
 public interface HospedeRepositorio extends JpaRepository<Hospede,Long> {
 
+
+    
     List<Hospede> findByUsercliente_Cpf(String Cpf);
+
+    
+
+
     
 }
