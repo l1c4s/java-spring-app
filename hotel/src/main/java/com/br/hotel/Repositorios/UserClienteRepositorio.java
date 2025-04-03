7699ebf4b31f69fbe40b8cdac1de265484577086
l1c4s <lucasferreirabarrosdallas@gmail.com>
2025-04-03 18:59:44 -0300

    Metodo finBycpf

--- hotel/src/main/java/com/br/hotel/Repositorios/UserClienteRepositorio.java
+++ hotel/src/main/java/com/br/hotel/Repositorios/UserClienteRepositorio.java
@@ -1,5 +1,7 @@
 package com.br.hotel.Repositorios;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.br.hotel.models.UserCliente;
 
 @Repository
 public interface UserClienteRepositorio extends JpaRepository<UserCliente, String> {
+
+    Optional<UserCliente> findByCpf(String cpf);
     
 }
