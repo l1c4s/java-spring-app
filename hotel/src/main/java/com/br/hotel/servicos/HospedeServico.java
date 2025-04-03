e092a87e324c9121f0c83b544a802ac882dbac99
l1c4s <lucasferreirabarrosdallas@gmail.com>
2025-04-03 18:58:12 -0300

    Metodo persist com cpfUsuario

--- hotel/src/main/java/com/br/hotel/servicos/HospedeServico.java
+++ hotel/src/main/java/com/br/hotel/servicos/HospedeServico.java
@@ -1,12 +1,18 @@
 package com.br.hotel.servicos;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.br.hotel.Repositorios.HospedeRepositorio;
+import com.br.hotel.Repositorios.UserClienteRepositorio;
 import com.br.hotel.models.Hospede;
+import com.br.hotel.models.UserCliente;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 
 @Service
 public class HospedeServico {
@@ -14,11 +20,23 @@ public class HospedeServico {
     @Autowired
     private HospedeRepositorio hospederepositorio;
 
-    public Hospede salvarHospede(Hospede hospede){
-        return hospederepositorio.save(hospede);
+    @Autowired
+    private UserClienteRepositorio userClienteRepositorio;
+   
+    public List<Hospede> ListarHospedes(){
+        return hospederepositorio.findAll();
     }
 
-    public List<Hospede> ListarHospedes(String UsuarioCpf){
-        return hospederepositorio.findByUsercliente_Cpf(UsuarioCpf);
-    }
+    @Transactional
+    public Hospede persist(Hospede hospede, String cpfUser) {
+    UserCliente usuario = userClienteRepositorio.findByCpf(cpfUser)
+                            .orElseThrow(()-> new RuntimeException("Usuario nao encontrrado"));
+
+    hospede.setUsercliente(usuario);
+    //usuario.getHospedes().add(hospede);
+    return hospederepositorio.save(hospede);
+}
+
+    
+    
 }
